f17747c1c18900407ee9218bbd834098415fa0f2
Ismael-Deka <ismael.deka7899@gmail.com>
2022-05-07 18:18:56 -0400

    Google Sign In Button Added

--- lib/screens/SignInScreen.dart
+++ lib/screens/SignInScreen.dart
@@ -2,6 +2,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sign_in_button/sign_in_button.dart';
 import 'package:trivial_trivia/services/auth.dart';
 
 class SignIn extends StatefulWidget {
@@ -74,170 +75,193 @@ class _SignInState extends State<SignIn> {
                         )),
                   ),
                 ),
-                Align(
-                    alignment: const Alignment(0, -0.55),
-                    child: Text('Welcome back',
+                Column(
+                  children: [
+                    SizedBox(
+                      height: size.height*0.2,
+                    ),
+                    Text('Welcome back',
                         style: GoogleFonts.inter(
                           color: const Color.fromRGBO(255, 255, 255, 1),
                           fontSize: 35,
                           fontWeight: FontWeight.w600,
                           fontStyle: FontStyle.italic,
-                        ))),
-                Stack(
-                  children: <Widget>[
-                    Align(
-                        alignment: const Alignment(0, 0.10),
-                        child: ClipRect(
-                            child: Container(
-                                height: 350,
-                                width: 315,
-                                decoration: BoxDecoration(
-                                    color: Colors.white.withOpacity(0.2),
-                                    borderRadius: BorderRadius.circular(30)),
-                                child: Column(
-                                  children: <Widget>[
-                                    SizedBox(
-                                      height: size.height*0.05,
-                                    ),
-                                    Text('Sign In',
-                                      style: GoogleFonts.inter(
-                                        color: const Color.fromRGBO(
-                                            255, 255, 255, 1),
-                                        fontSize: 35,
-                                        fontWeight: FontWeight.w400,
-                                        fontStyle: FontStyle.italic,
-                                      )),
-                                    SizedBox(
-                                      height: size.height*0.02,
-                                    ),
-                                    SizedBox(
-                                  height: 40,
-                                  width: 250,
-                                  child: TextField(
-                                    controller: emailController,
-                                    decoration: InputDecoration(
-                                        contentPadding:
-                                        const EdgeInsets.only(
-                                            left: 15, right: 15),
-                                        border: OutlineInputBorder(
-                                          borderSide: BorderSide.none,
-                                          borderRadius:
-                                          BorderRadius.circular(30),
-                                        ),
-                                        filled: true,
-                                        hintText: "Email",
-                                        hintStyle: const TextStyle(
-                                            fontSize: 13,
-                                            color: Color.fromRGBO(
-                                                255, 255, 255, 1)),
-                                        fillColor: Colors.white
-                                            .withOpacity(0.5)),
-                                  ),
-                                ),
-                                    SizedBox(
-                                      height: size.height*0.02,
-                                    ),
-                                    SizedBox(
-                                      height: 40,
-                                      width: 250,
-                                      child: TextField(
-                                        controller: passwordController,
-                                        obscureText: true,
-                                        decoration: InputDecoration(
-                                            contentPadding:
-                                            const EdgeInsets.only(
-                                                left: 15, right: 15),
-                                            border: OutlineInputBorder(
-                                              borderSide: BorderSide.none,
-                                              borderRadius:
-                                              BorderRadius.circular(30),
-                                            ),
-                                            filled: true,
-                                            hintText: "Password",
-                                            hintStyle: const TextStyle(
-                                                fontSize: 13,
-                                                color: Color.fromRGBO(
-                                                    255, 255, 255, 1)),
-                                            fillColor: Colors.white
-                                                .withOpacity(0.5)),
-                                      ),
-                                    ),
-                                    Row (
-                                      mainAxisAlignment: MainAxisAlignment.end,
-                                      children: [
-                                        GestureDetector(
-                                            onTap: () => Navigator.pushNamed(
-                                                context, '/resetpassword'),
-                                            child: Text('Forgot Password?',
-                                                style: GoogleFonts.inter(
-                                                  color: const Color.fromRGBO(
-                                                      255, 255, 255, 1),
-                                                  fontSize: 10,
-                                                  fontWeight: FontWeight.w600,
-                                                ))),
+                        )),
+                    SizedBox(
+                        height: size.height*0.02
+                    ),
+                    Stack(
+                      children: <Widget>[
+                        Align(
+                            alignment: const Alignment(0, 0.10),
+                            child: ClipRect(
+                                child: Container(
+
+                                    width: size.width*0.75,
+                                    decoration: BoxDecoration(
+                                        color: Colors.white.withOpacity(0.2),
+                                        borderRadius: BorderRadius.circular(30)),
+                                    child: Column(
+                                      children: <Widget>[
                                         SizedBox(
-                                          width: size.width*0.1,
+                                          height: size.height*0.05,
+                                        ),
+                                        Text('Sign In',
+                                            style: GoogleFonts.inter(
+                                              color: const Color.fromRGBO(
+                                                  255, 255, 255, 1),
+                                              fontSize: 35,
+                                              fontWeight: FontWeight.w400,
+                                              fontStyle: FontStyle.italic,
+                                            )),
+                                        SizedBox(
+                                          height: size.height*0.02,
+                                        ),
+                                        SizedBox(
+                                          height: 40,
+                                          width: 250,
+                                          child: TextField(
+                                            controller: emailController,
+                                            decoration: InputDecoration(
+                                                contentPadding:
+                                                const EdgeInsets.only(
+                                                    left: 15, right: 15),
+                                                border: OutlineInputBorder(
+                                                  borderSide: BorderSide.none,
+                                                  borderRadius:
+                                                  BorderRadius.circular(30),
+                                                ),
+                                                filled: true,
+                                                hintText: "Email",
+                                                hintStyle: const TextStyle(
+                                                    fontSize: 13,
+                                                    color: Color.fromRGBO(
+                                                        255, 255, 255, 1)),
+                                                fillColor: Colors.white
+                                                    .withOpacity(0.5)),
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          height: size.height*0.02,
+                                        ),
+                                        SizedBox(
+                                          height: 40,
+                                          width: 250,
+                                          child: TextField(
+                                            controller: passwordController,
+                                            obscureText: true,
+                                            decoration: InputDecoration(
+                                                contentPadding:
+                                                const EdgeInsets.only(
+                                                    left: 15, right: 15),
+                                                border: OutlineInputBorder(
+                                                  borderSide: BorderSide.none,
+                                                  borderRadius:
+                                                  BorderRadius.circular(30),
+                                                ),
+                                                filled: true,
+                                                hintText: "Password",
+                                                hintStyle: const TextStyle(
+                                                    fontSize: 13,
+                                                    color: Color.fromRGBO(
+                                                        255, 255, 255, 1)),
+                                                fillColor: Colors.white
+                                                    .withOpacity(0.5)),
+                                          ),
+                                        ),
+                                        Row (
+                                          mainAxisAlignment: MainAxisAlignment.end,
+                                          children: [
+                                            GestureDetector(
+                                                onTap: () => Navigator.pushNamed(
+                                                    context, '/resetpassword'),
+                                                child: Text('Forgot Password?',
+                                                    style: GoogleFonts.inter(
+                                                      color: const Color.fromRGBO(
+                                                          255, 255, 255, 1),
+                                                      fontSize: 10,
+                                                      fontWeight: FontWeight.w600,
+                                                    ))),
+                                            SizedBox(
+                                              width: size.width*0.1,
+                                            )
+                                          ],
+                                        ),
+                                        SizedBox(
+                                          height: size.height*0.02,
+                                        ),
+                                        GestureDetector(
+                                          onTap: loginUser,
+                                          child: Container(
+                                              width: 140,
+                                              height: 40,
+                                              decoration: BoxDecoration(
+                                                  borderRadius:
+                                                  BorderRadius.circular(30),
+                                                  color: Colors.white
+                                                      .withOpacity(0.5)),
+                                              child: Center(
+                                                child: _isLoading
+                                                    ? const CircularProgressIndicator(
+                                                    color: Color.fromRGBO(
+                                                        255, 255, 255, 1))
+                                                    : Text('Login',
+                                                    style: GoogleFonts.inter(
+                                                        color: const Color
+                                                            .fromRGBO(
+                                                            255, 255, 255, 1),
+                                                        fontSize: 15)),
+                                              )),
+
+                                        ),
+                                        Padding(
+                                          padding: EdgeInsets.fromLTRB(size.width*0.1, size.height*0.02, size.width*0.1, size.height*0.02),
+                                          child: const Divider(
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                        SignInButton(
+                                            Buttons.google,
+                                            shape: RoundedRectangleBorder(
+                                              borderRadius: BorderRadius.circular(5.0),
+                                            ),
+                                            onPressed: (){}
+                                        ),
+                                        SizedBox(
+                                          height: size.height*0.02,
                                         )
                                       ],
-                                    ),
-                                    SizedBox(
-                                      height: size.height*0.02,
-                                    ),
-                                    GestureDetector(
-                                      onTap: loginUser,
-                                      child: Container(
-                                          width: 140,
-                                          height: 40,
-                                          decoration: BoxDecoration(
-                                              borderRadius:
-                                              BorderRadius.circular(30),
-                                              color: Colors.white
-                                                  .withOpacity(0.5)),
-                                          child: Center(
-                                            child: _isLoading
-                                                ? const CircularProgressIndicator(
-                                                color: Color.fromRGBO(
-                                                    255, 255, 255, 1))
-                                                : Text('Login',
-                                                style: GoogleFonts.inter(
-                                                    color: const Color
-                                                        .fromRGBO(
-                                                        255, 255, 255, 1),
-                                                    fontSize: 15)),
-                                          )),
-
-                                    ),
-                                  ],
-                                )))),
-                  ],
-                ),
-                Align(
-                    alignment: const Alignment(0, 0.70),
-                    child: Text('Need an account?',
+                                    )))),
+                      ],
+                    ),
+                    SizedBox(
+                      height: size.height*0.05
+                    ),
+                    Text('Need an account?',
                         style: GoogleFonts.inter(
                           color: const Color.fromRGBO(255, 255, 255, 1),
                           fontSize: 20,
                           fontWeight: FontWeight.w600,
                           fontStyle: FontStyle.italic,
-                        ))),
-                Align(
-                  alignment: const Alignment(0, 0.80),
-                  child: GestureDetector(
-                    onTap: () => Navigator.pushNamed(context, '/registration'),
-                    child: Container(
-                        width: 130,
-                        height: 30,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(30),
-                            color: Colors.white.withOpacity(0.5)),
-                        child: Center(
-                          child: Text('Register',
-                              style: GoogleFonts.inter(
-                                  color: const Color.fromRGBO(255, 255, 255, 1),
-                                  fontSize: 15)),
                         )),
-                  ),
-                )
+                    GestureDetector(
+                      onTap: () => Navigator.pushNamed(context, '/registration'),
+                      child: Container(
+                          width: 130,
+                          height: 30,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(30),
+                              color: Colors.white.withOpacity(0.5)),
+                          child: Center(
+                            child: Text('Register',
+                                style: GoogleFonts.inter(
+                                    color: const Color.fromRGBO(255, 255, 255, 1),
+                                    fontSize: 15)),
+                          )),
+                    )
+                  ],
+                ),
+
               ]),
             )));
   }
